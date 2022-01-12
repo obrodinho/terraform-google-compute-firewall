1c49122447393cdc252d8a72c4afeb1249d1cb59
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-01-12 23:15:37 +0530

    initial commit

--- output.tf
+++ output.tf
@@ -1,9 +1,4 @@
-output "public_ip" {
-  description = "The internet facing public ip for the compute incstance"
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
-}
-
-output "compute_instance_serviceaccount" {
-  description = "The email/name of the compute instance service account"
-  value       = google_service_account.default.email
+output "id" {
+  description = "an identifier for the resource with format projects/{{project}}/global/firewalls/{{name}}"
+  value = google_compute_firewall.firewall.id
 }
