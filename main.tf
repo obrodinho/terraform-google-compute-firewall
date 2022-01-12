789162372863fc0d0d5d1b2ee4b6b0a0d84b70c6
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-01-13 00:14:02 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "google_compute_firewall" "firewall" {
 		}
   }
 
-  source_tags = var.direction == var.source_service_accounts != [] || "EGRESS" || var.source_tags == [] ? null : var.source_tags
-  target_tags = var.direction == var.target_service_accounts != [] || "INGRESS" || var.target_tags == [] ? null : var.target_tags
+  source_tags = var.source_service_accounts != [] || var.direction == "EGRESS" || var.source_tags == [] ? null : var.source_tags
+  target_tags = var.target_service_accounts != [] || var.direction == "INGRESS" || var.target_tags == [] ? null : var.target_tags
 	source_service_accounts = var.source_tags != [] || var.direction == "EGRESS" || var.source_service_accounts == [] ? null : var.source_service_accounts
 	target_service_accounts = var.target_tags != [] ||  var.direction == "INGRESS" || var.target_service_accounts == [] ? null : var.target_service_accounts
 }
