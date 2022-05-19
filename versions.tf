9bcd8a057878c48cf62e044b7fdbcc6ea5d6da85
Rafael "Brodinho" Guimarães <465634+obrodinho@users.noreply.github.com>
2022-05-19 15:38:08 -0300

    chore: Renames file and upgrades provider and terraform versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.1.2"
+  required_version = ">= 1.1.7"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.1.0"
+      version = "4.21.0"
     }
   }
 }
