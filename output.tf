6187fe148988b21c8bf49acce2f519474efb8db3
Rafael "Brodinho" Guimarães <465634+obrodinho@users.noreply.github.com>
2022-09-30 15:00:04 -0300

    feat: Adds source_ranges output
    Based on what was applied to the rule

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "An identifier for the resource with format `projects/{{project}}/global/firewalls/{{name}}`"
   value       = google_compute_firewall.this.id
 }
+
+output "source_ranges" {
+  description = "Sources applied on rule"
+  value       = google_compute_firewall.this.source_ranges
+}
